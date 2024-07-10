757c09056bb1ab9e339b9cf4c2b8a38435788150
nori1043 <seki_1043@icloud.com>
2024-07-10 11:20:09 +0900

    フォルダをアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+for (let number = 1; number <= 100; number++){
+
+  if(number %3 === 0){
+    console.log("3の倍数です")
+  }
+
+if(number %5 ===0){
+   console.log("5の倍数です")
+}
+
+
+if(number %15 ===0)
+   console.log("3と5の倍数です")
+
+
+else{
+  console.log(number)
+}
+
+}
